188ed5b3c236e6c7775d2cc1e20ef9141131074f
Nikolaos stathokostopoulos <13-13034@saeinstitution.edu>
2020-10-30 11:07:58 +0200

    Initial commit

---
+++ js/exName.js
@@ -0,0 +1,5 @@
+
+let firstName = prompt ('Ποιό ειναι το ονομά σου?');
+let lastName = prompt ('Ποιό ειναι το επιθετό σου?');
+let fullName = firstName + ' ' + lastName;
+document.write('Keep coding' + ' ' + fullName + '!' );
