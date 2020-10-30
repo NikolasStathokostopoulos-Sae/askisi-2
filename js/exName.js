c5b88f1ff2ed3d51ea463d676310468384447e34
Nikolaos stathokostopoulos <13-13034@saeinstitution.edu>
2020-10-30 13:08:31 +0200

    Update exName.js

--- js/exName.js
+++ js/exName.js
@@ -1,5 +1,8 @@
-
+'use strict';
 let firstName = prompt ('Ποιό ειναι το ονομά σου?');
 let lastName = prompt ('Ποιό ειναι το επιθετό σου?');
 let fullName = firstName + ' ' + lastName;
-document.write('Keep coding' + ' ' + fullName + '!' );
+let greeting = 'Keep coding';
+
+//Για να εμφανιστεί στην σελίδα και οχι σαν popup παράθυρο στον browser
+document.write( greeting + ' ' + fullName + '!' );
